ab2463a73a2f20b6e3e7aab502699a57a3fb952e
Roshka Anatoliy <roshka.anatoliy@gmail.com>
2019-03-28 08:44:36 +0100

    Add Navigation view with some lines. Created an activity_home_test, it contentains new grafics elements.

--- app/src/main/java/com/example/mytrainer/HomeActivity.kt
+++ app/src/main/java/com/example/mytrainer/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mytrainer
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import com.example.mytrainer.auth.Auth
@@ -8,16 +11,21 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : GeneralActivity("Home"), Toolbar.OnMenuItemClickListener {
 
+    private val LAYOUT: Int = R.layout.activity_home_test
     private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppDefaul)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        initToolbar()
+        setContentView(LAYOUT)
 
-        logout.setOnClickListener {
-            Auth(this).logout()
+        initToolbar()
+        initNavigationView()
+
+      /*  logout.setOnClickListener {
+           Auth(this).logout()
         }
+      */
     }
 
     private fun initToolbar(){
@@ -29,4 +37,26 @@ class HomeActivity : GeneralActivity("Home"), Toolbar.OnMenuItemClickListener {
     override fun onMenuItemClick(item: MenuItem?): Boolean {
         return false
     }
+
+    private fun initNavigationView() {
+        val drawerLayoyt: DrawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toogle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayoyt, this.toolbar, R.string.view_navigation_open, R.string.view_navigation_close)
+
+        drawerLayoyt.addDrawerListener(toogle)
+        toogle.syncState()
+
+        var navigationView = findViewById<NavigationView>(R.id.navigation)
+        navigationView.setNavigationItemSelectedListener{
+            drawerLayoyt.closeDrawers()
+            when(it.itemId){
+                R.id.actionNotificationItem -> {
+                    true
+                }
+
+                else -> true
+            }
+
+        }
+    }
+
 }
